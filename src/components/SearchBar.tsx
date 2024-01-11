f49075d60acd0c4af623a6f4cbf107a8ab8e18b3
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-11 14:19:38 +0800

    Wrap labels in form and wrap form in div to style

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,21 @@
 function SearchBar() {
   return (
     <>
-      <label htmlFor="search">
-        <input id="search" placeholder="Search..." />
-      </label>
-      <br />
-      <label htmlFor="stock">
-        <input id="stock" type="checkbox" />
-        Only show producs in stock
-      </label>
+      <div className="mt-2">
+        <form className="form-control">
+          <label htmlFor="search">
+            <input
+              id="search"
+              placeholder="Search..."
+              className="form-control"
+            />
+          </label>
+          <br />
+          <label htmlFor="stock">
+            <input id="stock" type="checkbox" /> Only show producs in stock
+          </label>
+        </form>
+      </div>
     </>
   );
 }
