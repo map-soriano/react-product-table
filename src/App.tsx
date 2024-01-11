025569a1f18a4652e11c3cd71f5a49982f308b26
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-11 12:25:06 +0800

    Create App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import ProductTable from "./components/ProductTable";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable products={JSON_RESPONSE} />
+    </>
+  );
+}
+
 const JSON_RESPONSE = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
   { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
@@ -6,3 +18,5 @@ const JSON_RESPONSE = [
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
 ];
+
+export default App;
