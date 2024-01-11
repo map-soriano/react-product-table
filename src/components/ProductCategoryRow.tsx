4905fd76e89e5b55505745b0500c0c228ed547fa
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-11 12:37:43 +0800

    Map each product in each category to ProductRow

--- src/components/ProductCategoryRow.tsx
+++ src/components/ProductCategoryRow.tsx
@@ -1,6 +1,20 @@
+import ProductRow from "./ProductRow";
+
 // TODO: Change ANY here to expect a dictionary
-function ProductCategoryRow({ product }: any) {
-  return <h6>{product.category}</h6>;
+function ProductCategoryRow(products: any) {
+  return (
+    <>
+      {/* TODO: Make CATEGORY h6 to be dynamic*/}
+      <h6>CATEGORY</h6>
+      {products.product.map((product: any) => (
+        <ProductRow
+          key={product.name}
+          name={product.name}
+          price={product.price}
+        />
+      ))}
+    </>
+  );
 }
 
 export default ProductCategoryRow;
