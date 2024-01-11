b967e035c9433f21723314febc8b8306dd669596
Ace <mpsoriano1@student.hau.edu.ph>
2024-01-11 12:30:58 +0800

    Comment out checkbox input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,7 @@ function SearchBar() {
   return (
     <>
       <input placeholder="Search..."></input>
-      <input type="checkbox">Only show products in stock</input>
+      {/* <input type="checkbox">Only show products in stock</input> */}
     </>
   );
 }
